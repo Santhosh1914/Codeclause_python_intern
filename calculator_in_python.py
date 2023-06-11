e4bfce8ecbd265452e0788a6db9796622a6e6bbd
Santhosh1914 <125819730+Santhosh1914@users.noreply.github.com>
2023-06-11 11:47:55 +0530

    Update calculator_in_python.py

--- calculator_in_python.py
+++ calculator_in_python.py
@@ -38,3 +38,56 @@ elif choice == '4':
     print("Result: ", result)
 else:
     print("Invalid choice!")
+    
+    
+#we can use these code also========
+
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Cannot divide by zero"
+    else:
+        return num1 / num2
+
+print("Welcome to the Calculator Program!")
+
+while True:
+    print("\nPlease select an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '5':
+        print("Thank you for using the Calculator Program!")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+    elif choice == '2':
+        result = subtract(num1, num2)
+    elif choice == '3':
+        result = multiply(num1, num2)
+    elif choice == '4':
+        result = divide(num1, num2)
+    else:
+        print("Invalid choice! Please try again.")
+        continue
+
+    print("Result: ", result)
+
